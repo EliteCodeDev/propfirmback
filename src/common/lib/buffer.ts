9ff5fd6d1959937ae009226fc24c5446a9750096
DanielArmas22 <carmasa@unitru.edu.pe>
2025-08-08 16:25:46 -0500

    feat: add buffer data class

--- src/common/lib/buffer.ts
+++ src/common/lib/buffer.ts
@@ -1,40 +1,38 @@
-import { Injectable } from "@nestjs/common";
-
+import { Injectable } from '@nestjs/common';
+import { Account } from '../utils/account';
 @Injectable() //una sola instancia del buffer estara en toda la aplicacion
-export class ContextBuffer{
+export class ContextBuffer {
+  private buffer: Map<string, Account> = new Map(); //login, objeto con infromacion
 
-    private buffer: Map <string, object> = new Map() //login, objeto con infromacion
+  public setBuffer(login: string, data: Account): void {
+    this.buffer.set(login, data);
+  }
 
-    public setBuffer(login:string, data:object): void{
-        this.buffer.set(login, data)
+  public getBuffer(login: string): Account | undefined {
+    return this.buffer.get(login);
+  }
+
+  public has(login: string): boolean {
+    return this.buffer.has(login);
+  }
+
+  public delete(login: string): void {
+    this.buffer.delete(login);
+  }
+
+  public clear(): void {
+    this.buffer.clear();
+  }
+
+  public getAllBuffers(): Record<string, Account> {
+    const obj: Record<string, Account> = {};
+    for (const [key, value] of this.buffer.entries()) {
+      obj[key] = value;
     }
+    return obj;
+  }
 
-    public getBuffer(login:string): object | undefined {
-        return this.buffer.get(login)
-    }
-
-    public has(login:string): boolean {
-        return this.buffer.has(login)
-    }
-
-    public delete(login:string): void {
-        this.buffer.delete(login)
-    }
-
-    public clear(): void{
-        this.buffer.clear()
-    }
-
-    public getAllBuffers(): Record<string, object> {
-        const obj: Record<string, object> = {};
-        for (const [key, value] of this.buffer.entries()) {
-            obj[key] = value;
-        }
-        return obj;
-    }
-
-    public getLength(): number {
-        return this.buffer.size;
-    }
-
+  public getLength(): number {
+    return this.buffer.size;
+  }
 }
--- src/common/utils/account.ts
+++ src/common/utils/account.ts
@@ -2,4 +2,16 @@ export class Account {
   userID: string;
   login: string;
   balance: number;
+  equity: number;
+
+  openPositions: Array<object>; // Assuming this is an object with position details
+
+  closedPositions: Array<object>; // Assuming this is an object with closed position details
+  lastUpdate: Date;
+
+  // data posterior al analisis de las posiciones
+
+  metaStats: Object;
+  validation: Object;
+
 }
