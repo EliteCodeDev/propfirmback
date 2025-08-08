542bc93568b871cb85d2d278a25ca888bf2f169d
Fabian Ruiz <fruizs@unitru.edu.pe>
2025-08-08 17:06:15 -0500

    types de account v1

--- src/common/utils/account.ts
+++ src/common/utils/account.ts
@@ -1,12 +1,15 @@
+import { DataOpenPositions, DataClosePositions } from "../types/mt4/data.type";
+
+
 export class Account {
   userID: string;
   login: string;
   balance: number;
   equity: number;
 
-  openPositions: Array<object>; // Assuming this is an object with position details
+  openPositions: DataOpenPositions; // Assuming this is an object with position details
 
-  closedPositions: Array<object>; // Assuming this is an object with closed position details
+  closedPositions: DataClosePositions; // Assuming this is an object with closed position details
   lastUpdate: Date;
 
   // data posterior al analisis de las posiciones
