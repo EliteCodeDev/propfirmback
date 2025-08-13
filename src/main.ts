3f10512a29bba0c538364a19689da4f89e3685c8
Jose Luis <joseluisjlgd123@gmail.com>
2025-08-13 11:19:02 -0500

    fixed: add main ts

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as nodeCrypto from 'crypto';
-// @ts-ignore
-global.crypto = nodeCrypto;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
