cc6d4b23bc66760870a7d42d21e4907baaefb3d2
Ivan Fernandez <ivanfer105esqgmail.com>
2025-08-13 09:51:33 -0500

    main con crypto

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as nodeCrypto from 'crypto';
+// @ts-ignore
+global.crypto = nodeCrypto;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
