073d27926651a9923bd385cd4d79e8c3b324e40e
LeoPro23 <lguimaray@unitru.edu.pe>
2025-08-23 03:09:33 -0500

    fix: withdrawals now support orchestration script (again)

--- src/modules/withdrawals/withdrawals.module.ts
+++ src/modules/withdrawals/withdrawals.module.ts
@@ -3,9 +3,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { WithdrawalsController } from './withdrawals.controller';
 import { WithdrawalsService } from './withdrawals.service';
 import { Withdrawal } from './entities/withdrawal.entity';
+import { ChallengesModule } from '../challenges/challenges.module';
+import { CertificatesModule } from '../certificates/certificates.module';
+import { ChallengeTemplatesModule } from '../challenge-templates/challenge-templates.module';
+import { BrokerAccountsModule } from '../broker-accounts/broker-accounts.module';
+import { MailerModule } from '../mailer/mailer.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Withdrawal])],
+  imports: [
+    TypeOrmModule.forFeature([Withdrawal]),
+    ChallengesModule,
+    CertificatesModule,
+    ChallengeTemplatesModule,
+    BrokerAccountsModule,
+    MailerModule,
+  ],
   controllers: [WithdrawalsController],
   providers: [WithdrawalsService],
   exports: [WithdrawalsService],
